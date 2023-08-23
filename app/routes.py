929cb8f16b174dcf384616faa9d1b8cc350aa6be
Emran <emrangawher@gmail.com>
2023-08-23 19:05:11 +0300

    coach dashboard updated comparison , text feedback

--- app/routes.py
+++ app/routes.py
@@ -389,6 +389,62 @@ def coach_dashboard():
     else:
         flash('Please log in as a coach.')
         return redirect('/login')
+
+@views.route('/compare_players')##############################################################
+def compare_players():
+    if 'user_id' in session:
+        coach_id = session.get('user_id')
+        coach = Coach.query.get(coach_id)
+
+        enrolled_players = Player.query.filter_by(CoachCode=coach.CoachCode).all()
+        return render_template('compare_players.html', coach=coach, enrolled_players=enrolled_players)
+    else:
+        flash('Please log in as a coach.')
+        return redirect('/login')
+
+@views.route('/player_details/<int:player_id>')
+def player_details_coach(player_id):
+    if 'user_id' in session and session.get('user_type') == 'coach':
+        coach_id = session['user_id']
+        coach = Coach.query.get(coach_id)
+        player = Player.query.get(player_id)
+        
+        if request.method == 'POST':
+            feedback_text = request.form.get('feedback')
+
+            # Update the player's feedback field
+            player.CoachFeedback = feedback_text
+            db.session.commit()
+
+            return redirect(url_for('views.player_details_coach', player_id=player_id))
+        days = ['Monday', 'Wednesday', 'Friday']
+        player_workouts = {}
+        for day in days:
+            player_workouts[day] = player.get_upcoming_workouts(day)
+        return render_template('player_details_coach.html', coach=coach, player=player, player_workouts=player_workouts)
+    else:
+        flash('Please log in as a coach.')
+        return redirect('/login')
+    
+@views.route('/provide_feedback/<int:player_id>', methods=['POST'])
+def provide_feedback(player_id):
+    if 'user_id' in session and session.get('user_type') == 'coach':
+        coach_id = session['user_id']
+        coach = Coach.query.get(coach_id)
+        player = Player.query.get(player_id)
+
+        if request.method == 'POST':
+            feedback_text = request.form.get('feedback')
+
+            # Update the player's feedback field
+            player.CoachFeedback = feedback_text
+            db.session.commit()
+
+            flash('Feedback submitted successfully.')
+            return redirect(url_for('views.player_details_coach', player_id=player_id))
+
+    flash('Please log in as a coach.')
+    return redirect('/login')
     
 @views.route('/edit_player_workouts/<int:player_id>', methods=['GET', 'POST'])
 def edit_player_workouts(player_id):
